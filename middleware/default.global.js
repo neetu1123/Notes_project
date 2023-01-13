ff5d123d4a6458941444e8a5ce900fefa7a76b93
neetu1123 <95036500+neetu1123@users.noreply.github.com>
2023-01-13 11:58:09 +0530

    update

--- middleware/default.global.js
+++ middleware/default.global.js
@@ -1,21 +1,18 @@
 import useCounterStore from "../stores";
 
-export default async function(to, from) {
-   const store = useCounterStore();
-   const notesList = store.filterData
-   
-  
-//   stop navigation
-   if(!notesList.length && to.path === "/notes/list") {
-      alert('list is empty, please fill the data !')
-      return navigateTo('/')
-   }
-   
-   if(to.name =='notes-id' && !notesList.length ){
-      return navigateTo('/404')
-   }
+export default function define(to, from) {
+  const store = useCounterStore();
+  const notesList = store.filterData;
 
-   return undefined
- }
+  //   stop navigation
+  if (!notesList.length && to.path === "/notes/list") {
+    return navigateTo("/");
+  }
+  console.log(to, from);
+  if (to.name === "notes-id" && !notesList.length) {
+    console.log("rote error");
+    return navigateTo("/404");
+  }
 
- 
+  return undefined;
+}
